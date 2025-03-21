1915a1db394a7bc80c56c5832e017d3b6fa4416a
Gerard Porras <gerardporras@gmail.com>
2025-03-21 17:47:20 +0100

    Refactor athletics page layout and enhance configuration settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,31 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
-import cloudflare from "@astrojs/cloudflare";
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['es', 'en', 'ca', 'nl'],
-    routing: {
-      prefixDefaultLocale: false
-    }
-  },
-  output: "server",
-  adapter: cloudflare()
+    integrations: [tailwind(), react()],
+    i18n: {
+        defaultLocale: 'en',
+        locales: ['es', 'en', 'ca', 'nl'],
+        routing: {
+            prefixDefaultLocale: false,
+        },
+    },
+    output: 'server',
+    adapter: cloudflare(),
+    vite: {
+        build: {
+            target: 'esnext',
+        },
+        optimizeDeps: {
+            include: ['react', 'react-dom'],
+        },
+        server: {
+            hmr: {
+                overlay: false,
+            },
+        },
+    },
 });
